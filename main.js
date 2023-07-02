a63cfc5b20c7ec699e15f4d5c39a76af00d7a5e6
koara-kuma <kelpmonster14@gmail.com>
2023-07-01 20:32:37 -0400

    toned down the difficulty

--- main.js
+++ main.js
@@ -22,34 +22,43 @@ client.on('message', async (message) => {
   } else if (command === 'ping') {
     message.channel.send('Pong!');
   } 
-  // using the given api create a quest command that will return a trivia question for the user to answer
   else if (command === 'quest') {
-    const response = await axios.get('https://opentdb.com/api.php?amount=1');
-    const question = he.decode(response.data.results[0].question);
-    const answer = he.decode(response.data.results[0].correct_answer);
-    embed = new Discord.MessageEmbed()
-      .setTitle('A quest has appeared!')
-      .setDescription(question)
-      .setFooter('Answer Correctly to complete the quest!');
-    message.channel.send(embed);
-    const filter = (m) => m.author.id === message.author.id;
-    message.channel
-      .awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
-      .then((collected) => {
-        if (collected.first().content.toLowerCase() === answer.toLowerCase()) {
-          message.channel.send('You have completed the quest!');
-        } else {
+    try {
+      const response = await axios.get('https://opentdb.com/api.php?amount=1&type=multiple');
+      const question = he.decode(response.data.results[0].question);
+      const choices = response.data.results[0].incorrect_answers.map(he.decode);
+      const correctAnswer = he.decode(response.data.results[0].correct_answer);
+  
+      choices.push(correctAnswer);
+      choices.sort(); // Sort the choices alphabetically
+  
+      const embed = new Discord.MessageEmbed()
+        .setTitle('A quest has appeared!')
+        .setDescription(question)
+        .addField('Choices', choices.join('\n'))
+        .setFooter('Answer correctly to complete the quest!');
+  
+      message.channel.send(embed);
+  
+      const filter = (m) => m.author.id === message.author.id;
+      message.channel
+        .awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+        .then((collected) => {
+          if (collected.first().content.toLowerCase() === correctAnswer.toLowerCase()) {
+            message.channel.send('You have completed the quest!');
+          } else {
+            message.channel.send('You have failed the quest!');
+          }
+        })
+        .catch((collected) => {
           message.channel.send('You have failed the quest!');
-        }
-      }
-      )
-      .catch((collected) => {
-        message.channel.send('You have failed the quest!');
-      }
-      );
-      
-    
+        });
+    } catch (error) {
+      console.error('Error fetching quest:', error);
+      message.channel.send('Oops! An error occurred while fetching the quest.');
+    }
   }
+  
   
   // Add more commands here if needed
 });
