5423d984d73d4ef96a9b9939b40cb66356918cb5
koara-kuma <kelpmonster14@gmail.com>
2023-07-22 16:15:20 -0400

    working on shop

--- commands/buy.js
+++ commands/buy.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { getBankData, updateBank, shop } = require('../functions');
+const {getBankData, updateBank, buyThis, shop} = require('../functions');
 
 module.exports = {
   name: 'buy',
@@ -12,12 +12,19 @@ module.exports = {
     const itemName = args[0];
     const amount = args[1] || 1;
 
-    const success = await buyThis(message.author, itemName, amount);
+    const success = await buyThis(message.author, itemName, amount, shop); // Pass the 'shop' data as an argument.
 
-    if (success) {
+    if (success[0]) { // Check the first element of the 'success' array to determine if the purchase was successful.
       message.channel.send(`You have successfully bought ${amount} ${itemName}!`);
     } else {
-      message.channel.send(`Oops! There was an error while buying ${amount} ${itemName}.`);
+      const errorCode = success[1];
+      if (errorCode === 1) {
+        message.channel.send(`Oops! The item "${itemName}" is not available in the shop.`);
+      } else if (errorCode === 2) {
+        message.channel.send(`Oops! You don't have enough money to buy ${amount} ${itemName}.`);
+      } else {
+        message.channel.send('Oops! There was an error while processing your request.');
+      }
     }
   }
 };
--- functions.js
+++ functions.js
@@ -1,77 +1,83 @@
 const fs = require('fs');
 
+const shop = [
+    { name:'Trivia-Coin', price: 50},
+    { name: 'Trivia-Cake', price: 100 }
+];
+
 async function getBankData() {
-  const rawdata = fs.readFileSync('bank.json');
-  const users = JSON.parse(rawdata);
-  return users;
+    const rawdata = fs.readFileSync('bank.json');
+    const users = JSON.parse(rawdata);
+    return users;
 }
-
+  
 async function updateBank(user, change = 0, mode = 'bank') {
-  const users = await getBankData();
-  users[String(user.id)][mode] += change;
-
-  fs.writeFileSync('bank.json', JSON.stringify(users));
-
-  const bal = [users[String(user.id)]["bank"]];
-  return bal;
+    const users = await getBankData();
+    users[String(user.id)][mode] += change;
+  
+    fs.writeFileSync('bank.json', JSON.stringify(users));
+  
+    const bal = [users[String(user.id)]["bank"]];
+    return bal;
 }
-
+  
 async function buyThis(user, item_name, amount, shop) {
-  item_name = itemname.toLowerCase();
-  let name = null;
-  for (let item of shop) {
-    let name = item["name"].toLowerCase();
-    if (name === itemname) {
-      name = name;
-      let price = item["price"];
-      break;
-    }
-  }
-
-  if (name_ === null) {
-    return [false, 1];
-  }
-
-  let cost = price * amount;
-
-  let users = await getBankData();
-
-  let bal = await updateBank(user);
-
-  if (bal[0] < cost) {
-    return [false, 2];
-  }
-
-  try {
-    let index = 0;
-    let t = null;
-    for (let thing of users[String(user.id)]['bag']) {
-      let n = thing['item'];
-      if (n === item_name) {
-        let old_amt = thing['amount'];
-        let new_amt = old_amt + amount;
-        users[String(user.id)]['bag'][index]['amount'] = new_amt;
-        t = 1;
+    item_name = itemname.toLowerCase();
+    let name = null;
+    let price = 0; // Initialize 'price' variable
+  
+    for (let item of shop) {
+      let name = item["name"].toLowerCase();
+      if (name === itemname) {
+        name = name;
+        price = item["price"]; // Assign the price here
         break;
       }
-      index += 1;
     }
-    if (t === null) {
+  
+    if (name_ === null) {
+      return [false, 1];
+    }
+  
+    let cost = price * amount;
+  
+    let users = await getBankData();
+  
+    let bal = await updateBank(user, 0, "bank"); // Initialize 'bal' with '0' in updateBank call
+  
+    if (bal[0] < cost) {
+      return [false, 2];
+    }
+  
+    try {
+      let index = 0;
+      let t = null;
+      for (let thing of users[String(user.id)]['bag']) {
+        let n = thing['item'];
+        if (n === item_name) {
+          let old_amt = thing['amount'];
+          let new_amt = old_amt + amount;
+          users[String(user.id)]['bag'][index]['amount'] = new_amt;
+          t = 1;
+          break;
+        }
+        index += 1;
+      }
+      if (t === null) {
+        let obj = {"item": item_name, "amount": amount};
+        users[String(user.id)]["bag"].push(obj);
+      }
+    } catch {
       let obj = {"item": item_name, "amount": amount};
-      users[String(user.id)]["bag"].push(obj);
+      users[String(user.id)]["bag"] = [obj];
     }
-  } catch {
-    let obj = {"item": item_name, "amount": amount};
-    users[String(user.id)]["bag"] = [obj];
-  }
-
-  fs.writeFileSync("bank.json", JSON.stringify(users));
-
-  await updateBank(user, cost * -1, "bank");
-
-  return [true, "Worked"];
+  
+    fs.writeFileSync("bank.json", JSON.stringify(users));
+  
+    await updateBank(user, cost * -1, "bank");
+  
+    return [true, "Worked"];
 }
-
 async function openAccount(user) {
   const users = await getBankData();
 
@@ -86,16 +92,11 @@ async function openAccount(user) {
   return true;
 }
 
-// Add the shop data here
-const shop = [
-  { name: 'Trivia-Coin', price: 50 },
-  { name: 'Trivia-Cake', price: 100 }
-];
 
 module.exports = {
+  shop,
   getBankData,
   updateBank,
   buyThis,
   openAccount,
-  shop
 };
